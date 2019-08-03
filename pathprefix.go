f2d380b19c8fdad56881e7985e3703189e5f6e13
fooofei <aihujianfei@gmail.com>
2019-08-03 22:49:38 +0800

    fmt by gopls

--- pathprefix.go
+++ pathprefix.go
@@ -1,65 +1,65 @@
 package goremoter
 
 import (
-    "context"
-    "fmt"
-    "io"
-    "strings"
+	"context"
+	"fmt"
+	"io"
+	"strings"
 )
 
 // PathPrefix is the command path prefix
 // If `cd /home` is prefix, then ls -al . command will be
 // `cd /home && ls -al .`
 type pathPrefix struct {
-    prefix  string
-    remoter *Remoter
+	prefix  string
+	remoter *Remoter
 }
 
 // WithPrefix return the PathPrefix
 func WithPrefix(prefix string, remoter *Remoter) *pathPrefix {
-    return &pathPrefix{
-        prefix:  prefix,
-        remoter: remoter,
-    }
+	return &pathPrefix{
+		prefix:  prefix,
+		remoter: remoter,
+	}
 }
 
 // Run with path prefix
 func (p *pathPrefix) Run(waitCtx context.Context, cmds []string, stdout io.Writer, stderr io.Writer) error {
-    cmdsWithPrefix := make([]string, 0)
-    for _, cmd := range cmds {
-        cmdsWithPrefix = append(cmdsWithPrefix,
-            fmt.Sprintf("cd %s && %s", p.prefix, cmd))
-    }
-    return p.remoter.Run(waitCtx, cmdsWithPrefix, stdout, stderr)
+	cmdsWithPrefix := make([]string, 0)
+	for _, cmd := range cmds {
+		cmdsWithPrefix = append(cmdsWithPrefix,
+			fmt.Sprintf("cd %s && %s", p.prefix, cmd))
+	}
+	return p.remoter.Run(waitCtx, cmdsWithPrefix, stdout, stderr)
 }
 
 // Output with path prefix
 func (p *pathPrefix) Output(waitCtx context.Context, cmd string) (string, []byte, error) {
-    return p.remoter.Output(waitCtx, fmt.Sprintf("cd %s && %s", p.prefix, cmd))
+	return p.remoter.Output(waitCtx, fmt.Sprintf("cd %s && %s", p.prefix, cmd))
 }
 
 // Put with path prefix
 func (p *pathPrefix) Put(waitCtx context.Context, local string, remote string) error {
-    if !strings.HasPrefix(remote, "/") {
-        // remote need to add prefix
-        v := p.prefix
-        if !strings.HasSuffix(v, "/") {
-            v = fmt.Sprintf("%s/", v)
-        }
-        remote = fmt.Sprintf("%s%s", v, remote)
-    }
-    return p.remoter.Put(waitCtx, local, remote)
+	if !strings.HasPrefix(remote, "/") {
+		// remote need to add prefix
+		v := p.prefix
+		if !strings.HasSuffix(v, "/") {
+			v = fmt.Sprintf("%s/", v)
+		}
+		remote = fmt.Sprintf("%s%s", v, remote)
+	}
+	return p.remoter.Put(waitCtx, local, remote)
 }
 
 // Get with path prefix
 func (p *pathPrefix) Get(waitCtx context.Context, remote string, local string) error {
-    if !strings.HasPrefix(remote, "/") {
-        // remote need to add prefix
-        v := p.prefix
-        if !strings.HasSuffix(v, "/") {
-            v = fmt.Sprintf("%s/", v)
-        }
-        remote = fmt.Sprintf("%s%s", v, remote)
-    }
-    return p.remoter.Get(waitCtx, remote, local)
+	if !strings.HasPrefix(remote, "/") {
+		// remote need to add prefix
+		v := p.prefix
+		if !strings.HasSuffix(v, "/") {
+			v = fmt.Sprintf("%s/", v)
+		}
+		remote = fmt.Sprintf("%s%s", v, remote)
+	}
+	return p.remoter.Get(waitCtx, remote, local)
 }
--- remoter.go
+++ remoter.go
@@ -1,14 +1,15 @@
 package goremoter
 
 import (
-    "context"
-    "fmt"
-    "github.com/pkg/sftp"
-    "golang.org/x/crypto/ssh"
-    "io"
-    "io/ioutil"
-    "net"
-    "os"
+	"context"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net"
+	"os"
+
+	"github.com/pkg/sftp"
+	"golang.org/x/crypto/ssh"
 )
 
 // 比 "golang.org/x/crypto/ssh" 的优势：依靠context 来做超时管理，随心所欲的结束任务
@@ -16,43 +17,43 @@ import (
 //    而且依旧没有能力使用 context 做即时退出
 
 type Remoter struct {
-    clt    *ssh.Client
-    closer io.Closer // 记录 ssh 底层使用的 TCP 连接
-    // 为了有能力通过 context 退出
-    Host string
-    User string
-    Port string
+	clt    *ssh.Client
+	closer io.Closer // 记录 ssh 底层使用的 TCP 连接
+	// 为了有能力通过 context 退出
+	Host string
+	User string
+	Port string
 }
 
 func (r *Remoter) wait(waitCtx context.Context) chan bool {
-    funcExit := make(chan bool, 1)
-    go func() {
-        select {
-        case <-funcExit:
-        case <-waitCtx.Done():
-            _ = r.closer.Close()
-        }
-    }()
-    return funcExit
+	funcExit := make(chan bool, 1)
+	go func() {
+		select {
+		case <-funcExit:
+		case <-waitCtx.Done():
+			_ = r.closer.Close()
+		}
+	}()
+	return funcExit
 }
 
 // Copyed from ssh.Dial(), add context
 func (r *Remoter) dialContext(waitCtx context.Context, network, addr string,
-    config *ssh.ClientConfig) (*ssh.Client, error) {
-    d := net.Dialer{}
-    conn, err := d.DialContext(waitCtx, network, addr)
-    if err != nil {
-        return nil, err
-    }
-    r.closer = conn
+	config *ssh.ClientConfig) (*ssh.Client, error) {
+	d := net.Dialer{}
+	conn, err := d.DialContext(waitCtx, network, addr)
+	if err != nil {
+		return nil, err
+	}
+	r.closer = conn
 
-    noNeedWait := r.wait(waitCtx)
-    c, chans, reqs, err := ssh.NewClientConn(conn, addr, config)
-    close(noNeedWait)
-    if err != nil {
-        return nil, err
-    }
-    return ssh.NewClient(c, chans, reqs), nil
+	noNeedWait := r.wait(waitCtx)
+	c, chans, reqs, err := ssh.NewClientConn(conn, addr, config)
+	close(noNeedWait)
+	if err != nil {
+		return nil, err
+	}
+	return ssh.NewClient(c, chans, reqs), nil
 }
 
 // Dial remote machine
@@ -62,66 +63,66 @@ func (r *Remoter) dialContext(waitCtx context.Context, network, addr string,
 // sshConf["port"] =
 // sshConf["user"] =
 func Dial(waitCtx context.Context, sshConf map[string]interface{}) (*Remoter, error) {
-    r := &Remoter{}
-    var exists bool
-    r.Host, exists = sshConf["host"].(string)
-    if !exists {
-        return nil, fmt.Errorf("conf not exists host")
-    }
-    r.User, exists = sshConf["user"].(string)
-    if !exists {
-        return nil, fmt.Errorf("conf not exists user")
-    }
-    r.Port, exists = sshConf["port"].(string)
-    if !exists {
-        return nil, fmt.Errorf("conf not exists port")
-    }
-    addr := net.JoinHostPort(r.Host, r.Port)
+	r := &Remoter{}
+	var exists bool
+	r.Host, exists = sshConf["host"].(string)
+	if !exists {
+		return nil, fmt.Errorf("conf not exists host")
+	}
+	r.User, exists = sshConf["user"].(string)
+	if !exists {
+		return nil, fmt.Errorf("conf not exists user")
+	}
+	r.Port, exists = sshConf["port"].(string)
+	if !exists {
+		return nil, fmt.Errorf("conf not exists port")
+	}
+	addr := net.JoinHostPort(r.Host, r.Port)
 
-    auth := make([]ssh.AuthMethod, 0)
-    if pass, ok := sshConf["password"].(string); ok {
-        auth = append(auth, ssh.Password(pass))
-    } else {
-        privKeyFileName, exists := sshConf["privKey"].(string)
-        if !exists {
-            return nil, fmt.Errorf("conf not exists privKey")
-        }
-        privKeyBytes, err := ioutil.ReadFile(privKeyFileName)
-        if err != nil {
-            return nil, err
-        }
-        privKey, err := ssh.ParsePrivateKey(privKeyBytes)
-        if err != nil {
-            return nil, err
-        }
-        auth = append(auth, ssh.PublicKeys(privKey))
-    }
+	auth := make([]ssh.AuthMethod, 0)
+	if pass, ok := sshConf["password"].(string); ok {
+		auth = append(auth, ssh.Password(pass))
+	} else {
+		privKeyFileName, exists := sshConf["privKey"].(string)
+		if !exists {
+			return nil, fmt.Errorf("conf not exists privKey")
+		}
+		privKeyBytes, err := ioutil.ReadFile(privKeyFileName)
+		if err != nil {
+			return nil, err
+		}
+		privKey, err := ssh.ParsePrivateKey(privKeyBytes)
+		if err != nil {
+			return nil, err
+		}
+		auth = append(auth, ssh.PublicKeys(privKey))
+	}
 
-    hostKeyCallback := func(hostname string, remote net.Addr, key ssh.PublicKey) error {
-        return nil
-    }
+	hostKeyCallback := func(hostname string, remote net.Addr, key ssh.PublicKey) error {
+		return nil
+	}
 
-    cltConf := ssh.ClientConfig{
-        User:            r.User,
-        Auth:            auth,
-        HostKeyCallback: hostKeyCallback,
-    }
-    // 这里有 TCP 三次握手超时和 SSH 握手超时
-    clt, err := r.dialContext(waitCtx, "tcp", addr, &cltConf)
-    if err != nil {
-        return nil, err
-    }
-    r.clt = clt
-    return r, nil
+	cltConf := ssh.ClientConfig{
+		User:            r.User,
+		Auth:            auth,
+		HostKeyCallback: hostKeyCallback,
+	}
+	// 这里有 TCP 三次握手超时和 SSH 握手超时
+	clt, err := r.dialContext(waitCtx, "tcp", addr, &cltConf)
+	if err != nil {
+		return nil, err
+	}
+	r.clt = clt
+	return r, nil
 }
 
 // Close the ssn client
 func (r *Remoter) Close() error {
-    r.closer = nil
-    if r.clt != nil {
-        return r.clt.Close()
-    }
-    return nil
+	r.closer = nil
+	if r.clt != nil {
+		return r.clt.Close()
+	}
+	return nil
 }
 
 // Run commands
@@ -131,122 +132,122 @@ func (r *Remoter) Close() error {
 // diff with Output() method, all command share one same stdout
 func (r *Remoter) Run(waitCtx context.Context, cmds []string, stdout io.Writer, stderr io.Writer) error {
 
-    clt := r.clt
-    ssn, err := clt.NewSession()
-    if err != nil {
-        return err
-    }
-    defer ssn.Close()
+	clt := r.clt
+	ssn, err := clt.NewSession()
+	if err != nil {
+		return err
+	}
+	defer ssn.Close()
 
-    // not know why use this
-    // keep it for someday I know this
-    //modes := ssh.TerminalModes{
-    //    ssh.ECHO:          0,     // disable echoing
-    //    ssh.TTY_OP_ISPEED: 14400, // input speed = 14.4kbaud
-    //    ssh.TTY_OP_OSPEED: 14400, // output speed = 14.4kbaud
-    //}
-    //_ = modes
-    //if err := ssn.RequestPty("xterm", 80, 40, modes); err != nil {
-    //    return nil
-    //}
+	// not know why use this
+	// keep it for someday I know this
+	//modes := ssh.TerminalModes{
+	//    ssh.ECHO:          0,     // disable echoing
+	//    ssh.TTY_OP_ISPEED: 14400, // input speed = 14.4kbaud
+	//    ssh.TTY_OP_OSPEED: 14400, // output speed = 14.4kbaud
+	//}
+	//_ = modes
+	//if err := ssn.RequestPty("xterm", 80, 40, modes); err != nil {
+	//    return nil
+	//}
 
-    shellInput, err := ssn.StdinPipe()
-    if err != nil {
-        return err
-    }
-    ssn.Stdout = stdout
-    ssn.Stderr = stderr
-    err = ssn.Shell()
-    if err != nil {
-        return err
-    }
+	shellInput, err := ssn.StdinPipe()
+	if err != nil {
+		return err
+	}
+	ssn.Stdout = stdout
+	ssn.Stderr = stderr
+	err = ssn.Shell()
+	if err != nil {
+		return err
+	}
 
-    for _, cmd := range cmds {
-        _, err = fmt.Fprintf(shellInput, "%s\n", cmd)
-        if err != nil {
-            return err
-        }
-    }
-    // we must have exit to return `Wait()`
-    _, err = fmt.Fprintf(shellInput, "exit\n")
-    if err != nil {
-        return err
-    }
-    // all command after exit will not run
-    noNeedWait := r.wait(waitCtx)
-    err = ssn.Wait()
-    close(noNeedWait)
-    return err
+	for _, cmd := range cmds {
+		_, err = fmt.Fprintf(shellInput, "%s\n", cmd)
+		if err != nil {
+			return err
+		}
+	}
+	// we must have exit to return `Wait()`
+	_, err = fmt.Fprintf(shellInput, "exit\n")
+	if err != nil {
+		return err
+	}
+	// all command after exit will not run
+	noNeedWait := r.wait(waitCtx)
+	err = ssn.Wait()
+	close(noNeedWait)
+	return err
 }
 
 // Output run a command, and return stdout && stderr
 // Also return the executed command, when cmd is compose by prefix,
 // we need know cmd in caller
 func (r *Remoter) Output(waitCtx context.Context, cmd string) (string, []byte, error) {
-    // cannot use defer close(r.wait()) , defer may delay called,
-    // and cause a race condition
-    clt := r.clt
-    ssn, err := clt.NewSession()
-    if err != nil {
-        return cmd, nil, err
-    }
-    // stdout stderr all in one
-    noNeedWait := r.wait(waitCtx)
-    b, err := ssn.CombinedOutput(cmd)
-    close(noNeedWait)
-    _ = ssn.Close()
-    return cmd, b, err
+	// cannot use defer close(r.wait()) , defer may delay called,
+	// and cause a race condition
+	clt := r.clt
+	ssn, err := clt.NewSession()
+	if err != nil {
+		return cmd, nil, err
+	}
+	// stdout stderr all in one
+	noNeedWait := r.wait(waitCtx)
+	b, err := ssn.CombinedOutput(cmd)
+	close(noNeedWait)
+	_ = ssn.Close()
+	return cmd, b, err
 }
 
 // Put local file to remote, remove remote first in case of exists
 func (r *Remoter) Put(waitCtx context.Context, local string, remote string) error {
-    clt, err := sftp.NewClient(r.clt)
-    if err != nil {
-        return err
-    }
-    // ignore error remove first
-    _ = clt.Remove(remote)
-    rf, err := os.Open(local)
-    if err != nil {
-        _ = clt.Close()
-        return err
-    }
-    wf, err := clt.Create(remote)
+	clt, err := sftp.NewClient(r.clt)
+	if err != nil {
+		return err
+	}
+	// ignore error remove first
+	_ = clt.Remove(remote)
+	rf, err := os.Open(local)
+	if err != nil {
+		_ = clt.Close()
+		return err
+	}
+	wf, err := clt.Create(remote)
 
-    if err != nil {
-        _ = clt.Close()
-        _ = rf.Close()
-        return err
-    }
-    _, err = io.Copy(wf, rf)
-    _ = rf.Close()
-    _ = wf.Close()
-    _ = clt.Close()
-    return err
+	if err != nil {
+		_ = clt.Close()
+		_ = rf.Close()
+		return err
+	}
+	_, err = io.Copy(wf, rf)
+	_ = rf.Close()
+	_ = wf.Close()
+	_ = clt.Close()
+	return err
 }
 
 // Get remote file to local, remove local file first in case of exists
 func (r *Remoter) Get(waitCtx context.Context, remote string, local string) error {
-    clt, err := sftp.NewClient(r.clt)
-    if err != nil {
-        return err
-    }
+	clt, err := sftp.NewClient(r.clt)
+	if err != nil {
+		return err
+	}
 
-    _ = os.Remove(local)
-    rf, err := clt.Open(remote)
-    if err != nil {
-        _ = clt.Close()
-        return err
-    }
-    wf, err := os.Create(local)
-    if err != nil {
-        _ = clt.Close()
-        _ = rf.Close()
-        return err
-    }
-    _, err = io.Copy(wf, rf)
-    _ = wf.Close()
-    _ = rf.Close()
-    _ = clt.Close()
-    return err
+	_ = os.Remove(local)
+	rf, err := clt.Open(remote)
+	if err != nil {
+		_ = clt.Close()
+		return err
+	}
+	wf, err := os.Create(local)
+	if err != nil {
+		_ = clt.Close()
+		_ = rf.Close()
+		return err
+	}
+	_, err = io.Copy(wf, rf)
+	_ = wf.Close()
+	_ = rf.Close()
+	_ = clt.Close()
+	return err
 }
